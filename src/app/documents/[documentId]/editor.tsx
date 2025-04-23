777cd84a5fdf22cc907dabdf0559605626d053b2
rparekh01 <rajparekh001@outlook.com>
2025-04-22 23:51:57 -0400

    add initial extension and styles

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,16 @@
 "use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
-import { StarterKit } from "@tiptap/starter-kit";
+import StarterKit from "@tiptap/starter-kit";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
+
 export const Editor = () => {
   const editor = useEditor({
     editorProps: {
@@ -11,8 +20,38 @@ export const Editor = () => {
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Start Here...</p>",
+    extensions: [
+      StarterKit,
+      TaskItem.configure({ nested: true }),
+      TaskList.configure(),
+      Image,
+      ImageResize,
+      Table.configure({
+        HTMLAttributes: {
+          class: "my-custom-class",
+          resizable: true,
+        },
+      }),
+      TableHeader,
+      TableRow,
+      TableCell,
+    ],
+    content: `
+    <table>
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <th colspan="3">Description</th>
+        </tr>
+        <tr>
+          <td>Cyndi Lauper</td>
+          <td>Singer</td>
+          <td>Songwriter</td>
+          <td>Actress</td>
+        </tr>
+      </tbody>
+    </table>
+  `,
   });
 
   return (
