61104b619af433567690198e7fa8ef3f937c37da
rparekh01 <rajparekh001@outlook.com>
2025-05-03 10:25:59 -0400

    menubar

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -1,9 +1,48 @@
 import Image from "next/image";
+import Link from "next/link";
+import { DocumentInput } from "./document-input";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { FileIcon } from "lucide-react";
 
 export const Navbar = () => {
   return (
-    <nav>
-      <Image src="/logo.svg" alt="Logo placeholder" width={36} height={36} />
+    <nav className="flex items-center justify-between">
+      <div className="flex gap-2 items-center">
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            alt="Logo placeholder"
+            width={36}
+            height={36}
+          />
+        </Link>
+        <div>
+          <DocumentInput />
+          <div className="flex">
+            <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
+              <MenubarMenu>
+                <MenubarTrigger>
+                  File
+                  <MenubarContent>
+                    <MenubarItem>
+                      <FileIcon className="size-4 mr-2" />
+                      Save
+                    </MenubarItem>
+                  </MenubarContent>
+                </MenubarTrigger>
+              </MenubarMenu>
+            </Menubar>
+          </div>
+          {/* DocumentInput */}
+          {/* MenuBar */}
+        </div>
+      </div>
     </nav>
   );
 };
