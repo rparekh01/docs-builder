14d771006af7c3d00e4a152ed2d4880a663edf93
rparekh01 <rajparekh001@outlook.com>
2025-07-23 21:59:34 -0400

    fix it

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -41,8 +41,10 @@ export const Editor = ({ initialContent }: EditorProps) => {
   });
   const { setEditor } = useEditorStore();
 
-  const leftMargin = useStorage((root) => root.leftMargin);
-  const rightMargin = useStorage((root) => root.rightMargin);
+  const leftMargin =
+    useStorage((root) => root.leftMargin) ?? LEFT_PADDING_DEFAULT;
+  const rightMargin =
+    useStorage((root) => root.rightMargin) ?? RIGHT_PADDING_DEFAULT;
 
   const editor = useEditor({
     immediatelyRender: false,
@@ -73,7 +75,7 @@ export const Editor = ({ initialContent }: EditorProps) => {
 
     editorProps: {
       attributes: {
-        style: `padding-left:${leftMargin ?? LEFT_PADDING_DEFAULT}px ; padding-right:${rightMargin ?? RIGHT_PADDING_DEFAULT}px`,
+        style: `padding-left:${leftMargin}px ; padding-right:${rightMargin}px`,
         class:
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
